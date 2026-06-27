6d2cad8e6c2897766377a5a0983ac087c79ccac0
maverikkk-cmyk <maverikkk@gmail.com>
2026-06-27 22:27:47 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+# config.py
+
+API_ID = 1234567          # my.telegram.org se lein
+API_HASH = "your_hash"    # my.telegram.org se lein
+BOT_TOKEN = "your_token"  # @BotFather se lein
+GEMINI_KEY = "your_key"   # Google AI Studio se lein
+
+# Jo users bot ko fully control kar sakte hain (Admins)
+SUDO_USERS = [123456789] 
